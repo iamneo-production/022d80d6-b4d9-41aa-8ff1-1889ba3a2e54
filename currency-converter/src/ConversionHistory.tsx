439ab938ed11f76f146c92573175c2a4192274d8
heymeowcat <heyvidura@gmail.com>
2023-08-12 14:32:28 +0000

    fix: added table scroll and revesed ordering

--- currency-converter/src/ConversionHistory.tsx
+++ currency-converter/src/ConversionHistory.tsx
@@ -17,42 +17,43 @@ function ConversionHistory() {
   useEffect(() => {
     CurrencyService.getConversionHistory()
       .then(response => {
-        setConversionHistory(response.data);
+        setConversionHistory(response.data.reverse());
       })
       .catch(error => {
         console.error('Error fetching conversion history:', error);
       });
   }, []);
 
-
   return (
     <Card sx={{ minWidth: 300, p: 2 }}>
       <CardContent>
         <h2>Conversion History</h2>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Timestamp</TableCell>
-                <TableCell>Source Currency</TableCell>
-                <TableCell>Target Currency</TableCell>
-                <TableCell>Source Amount</TableCell>
-                <TableCell>Converted Amount</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {conversionHistory.map(history => (
-                <TableRow key={history.id}>
-                  <TableCell>{history.timestamp}</TableCell>
-                  <TableCell>{history.sourceCurrency}</TableCell>
-                  <TableCell>{history.targetCurrency}</TableCell>
-                  <TableCell>{history.sourceAmount}</TableCell>
-                  <TableCell>{history.convertedAmount}</TableCell>
+        <div style={{ maxHeight: '200px', overflowY: 'scroll' }}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Timestamp</TableCell>
+                  <TableCell>Source Currency</TableCell>
+                  <TableCell>Target Currency</TableCell>
+                  <TableCell>Source Amount</TableCell>
+                  <TableCell>Converted Amount</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {conversionHistory.map(history => (
+                  <TableRow key={history.id}>
+                    <TableCell>{history.timestamp}</TableCell>
+                    <TableCell>{history.sourceCurrency}</TableCell>
+                    <TableCell>{history.targetCurrency}</TableCell>
+                    <TableCell>{history.sourceAmount}</TableCell>
+                    <TableCell>{history.convertedAmount}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </CardContent>
     </Card>
   );
