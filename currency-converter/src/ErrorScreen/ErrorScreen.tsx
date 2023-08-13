d79a8406ea0bfc678b2b7cc429e0d4576e5c05ce
VikramSingh100245 <Vikramsingh100245@gmail.com>
2023-08-13 09:50:12 +0000

    made some chnages

--- currency-converter/src/ErrorScreen/ErrorScreen.tsx
+++ currency-converter/src/ErrorScreen/ErrorScreen.tsx
@@ -40,30 +40,3 @@ const styles: Styles = {
 };
 
 export default ErrorScreen;
-
-// CSS keyframes converted to TypeScript
-declare module 'react' {
-  interface CSSProperties {
-    animation?: string;
-  }
-}
-
-// Define the CSS keyframes
-const rainbowKeyframes = `
-  @keyframes rainbow {
-    0% { color: red; }
-    14% { color: orange; }
-    28% { color: yellow; }
-    42% { color: green; }
-    57% { color: blue; }
-    71% { color: indigo; }
-    85% { color: violet; }
-    100% { color: red; }
-  }
-`;
-
-// Add the keyframes to a <style> element in the document head
-const style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = rainbowKeyframes;
-document.head.appendChild(style);
