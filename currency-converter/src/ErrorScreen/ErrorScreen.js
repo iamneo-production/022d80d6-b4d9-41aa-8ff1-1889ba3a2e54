f79f48668bf35fabadea5a43487072cb7089b8bc
VikramSingh100245 <Vikramsingh100245@gmail.com>
2023-08-13 09:35:47 +0000

    Created a ErrorScreen

--- currency-converter/src/ErrorScreen/ErrorScreen.js
+++
@@ -1,7 +0,0 @@
-import react from 'react';
-function ErrorScreen(){
-    return (
-        <div>Error</div>
-    )
-}
-export default ErrorScreen;
---
+++ currency-converter/src/ErrorScreen/ErrorScreen.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ErrorScreen: React.FC = () => {
+  return (
+    <div id='error_div' style={styles.errorDiv}>
+      <h1 style={styles.errorHeading}>404 Page Not Found</h1>
+      <div id='error_base_div' style={styles.errorBaseDiv}>
+        <h2 style={styles.rainbowText}>Oh no!</h2>
+        <h3 style={styles.rainbowText}>Looks like you are lost.</h3>
+      </div>
+    </div>
+  );
+}
+
+const styles: { [key: string]: React.CSSProperties } = {
+  errorDiv: {
+    textAlign: 'center',
+    marginTop: '100px',
+  },
+  errorHeading: {
+    color: 'black',
+    fontSize: '48px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  errorBaseDiv: {
+    marginTop: '20px',
+  },
+  rainbowText: {
+    fontSize: '24px',
+    fontFamily: 'Arial, sans-serif',
+    animation: 'rainbow 2s infinite',
+  },
+};
+
+export default ErrorScreen;
