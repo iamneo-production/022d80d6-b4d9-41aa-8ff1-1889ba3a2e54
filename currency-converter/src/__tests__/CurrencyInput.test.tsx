788582737b21decd980104556c437431ae690070
heymeowcat <heyvidura@gmail.com>
2023-08-13 16:39:24 +0000

    test: test mock resolve values functions added

--- currency-converter/src/__tests__/CurrencyInput.test.tsx
+++ currency-converter/src/__tests__/CurrencyInput.test.tsx
@@ -1,10 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // Import this to extend expect with toBeInTheDocument
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import CurrencyInput from '../CurrencyInput';
 
-test('renders CurrencyInput component', () => {
-  render(<CurrencyInput />);
+jest.mock('../assets/mock-data/currencies.json')
 
-  expect(screen.getByText('Currency Converter')).toBeInTheDocument();
-  expect(screen.getByText('Convert')).toBeInTheDocument();
+describe('CurrencyInput component', () => {
+  let mockGetCurrencies: jest.Mock;
+  let mockPerformConversion: jest.Mock;
+  let mockAddConversionHistory: jest.Mock;
+  let mockGetConversionHistory: jest.Mock;
+  let mockUpdateExchangeRates: jest.Mock;
+
+  beforeEach(() => {
+    mockGetCurrencies = jest.fn();
+    mockGetCurrencies.mockResolvedValue({ data: [{ code: 'USD', name: 'US Dollar' }] });
+
+    mockPerformConversion = jest.fn();
+    mockPerformConversion.mockResolvedValue({ data: { convertedAmount: 100 } });
+
+    mockAddConversionHistory = jest.fn();
+    mockAddConversionHistory.mockResolvedValue({});
+
+    mockGetConversionHistory = jest.fn();
+    mockGetConversionHistory.mockResolvedValue({ data: [] });
+
+    mockUpdateExchangeRates = jest.fn();
+    mockUpdateExchangeRates.mockResolvedValue({});
+
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('displays initial currency input fields and buttons', () => {
+    render(<CurrencyInput setConversionHistory={jest.fn()} />);
+    
+    expect(screen.getByText('Currency Converter')).toBeInTheDocument();
+    expect(screen.getByText('Convert')).toBeInTheDocument();
+  });
+
 });
